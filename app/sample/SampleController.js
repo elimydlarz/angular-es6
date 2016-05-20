5ba8c1c880868ab605e193da949e55d02a060480
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-05-20 15:44:08 +0800

    Restructuring function declarations in classes.

--- app/sample/SampleController.js
+++ app/sample/SampleController.js
@@ -1,8 +1,11 @@
 import EngineClient from '../services/EngineClient';
 
 export default class SampleController {
-  constructor() {
-    this.getApplication = (applicationId) => new EngineClient().getApplication(applicationId);
-    this.getText = () => 'Hello everybody or some such string.';
+  getApplication(applicationId) {
+    return new EngineClient(this.$http).getApplication(applicationId);
+  }
+
+  getText() {
+    return 'Hello everybody or some such string.';
   }
 }
